91c47e5ab25f7737c928a3d9c94863433cf0bc13
reidfluegel <reid.fluegel@gmail.com>
2024-04-03 11:17:17 -0500

    manually set x axis ticks, add axis labels, reduce point size, make line green, adjust y axis range to include data

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -10,12 +10,6 @@
     let banks = [];
     let link_token = '';
     let handler;
-    let data = [
-        {date: '2023-01-01', epochTimestamp: 1672531200, value: 1000, previousPointPositive: true, realPoint: true},
-        {date: '2023-01-16', epochTimestamp: (1672531200 + 1675209600) / 2, value: 0, realPoint: false},
-        {date: '2023-02-01', epochTimestamp: 1675209600, value: -1500},
-        {date: '2023-03-01', epochTimestamp: 1677628800, value: 2000},
-    ].map(d => ({date: new Date(d.epochTimestamp), value: d.value}));
 
     onMount(async () => {
         if ((await axios.get('/api/users/is-verified')).data.isVerified) {
@@ -38,6 +32,22 @@
     })
 
     function drawChart() {
+        let rawData = [
+            {epochTimestamp: 1609459200000, value: 1000},
+            {epochTimestamp: 1640995200000, value: -1500},
+            {epochTimestamp: 1672531200000, value: 2000},
+        ];
+
+        let data = rawData.map(d => ({date: new Date(d.epochTimestamp), value: d.value}));
+
+        const oldestDate = data[0].date;
+        const newestDate = data[data.length - 1].date;
+        const range = rawData[rawData.length - 1].epochTimestamp - rawData[0].epochTimestamp;
+        const twoTenthDate = new Date(rawData[0].epochTimestamp + range * 0.2);
+        const fourTenthDate = new Date(rawData[0].epochTimestamp + range * 0.4);
+        const sixTenthDate = new Date(rawData[0].epochTimestamp + range * 0.6);
+        const eightTenthDate = new Date(rawData[0].epochTimestamp + range * 0.8);
+
         const margin = {top: 100, right: 100, bottom: 100, left: 100},
             width = 600 - margin.left - margin.right,
             height = 400 - margin.top - margin.bottom;
@@ -47,12 +57,67 @@
             .append('g')
             .attr('transform', `translate(${margin.left},${margin.top})`);
 
+        const minValue = d3.min(data, d => d.value);
+        const maxValue = d3.max(data, d => d.value);
+
+        // Determine the scale factor
+        const minScaleFactor = minValue < 0 ? 1.1 : 0.9;
+        const maxScaleFactor = maxValue < 0 ? 0.9 : 1.1;
+
         const x = d3.scaleTime().range([0, width]).domain(d3.extent(data, d => d.date));
-        const y = d3.scaleLinear().range([height, 0]).domain([d3.min(data, d => d.value) * 1.1, d3.max(data, d => d.value) * 1.1]);
+        const y = d3.scaleLinear()
+            .range([height, 0])
+            .domain([minValue * minScaleFactor, maxValue * maxScaleFactor]);
 
         const line = d3.line().x(d => x(d.date)).y(d => y(d.value));
 
-        // Tooltip setup
+        // Add a neutral line at 0 net worth
+        svg.append("line")
+            .attr("x1", 0)
+            .attr("x2", width)
+            .attr("y1", y(0))
+            .attr("y2", y(0))
+            .attr("stroke-width", 2)
+            .attr("stroke", "black")
+            .style("stroke-dasharray", ("3, 3"));
+
+        // Draw line
+        svg.append("path")
+            .data([data])
+            .attr("class", "line")
+            .attr("d", line)
+            .style("fill", "none")
+            .style("stroke", "green")
+            .style("stroke-width", "2px");
+
+        // Add the X Axis
+        svg.append('g')
+            .attr('transform', `translate(0,${height})`)
+            .call(d3.axisBottom(x)
+                .tickValues([oldestDate, twoTenthDate, fourTenthDate, sixTenthDate, eightTenthDate, newestDate])
+                .tickFormat(d3.timeFormat('%Y-%m-%d')))
+            .append('text')
+            .attr('class', 'axis-label')
+            .attr('x', width / 2) // Center the label
+            .attr('y', margin.bottom - 20) // Position below the x-axis line
+            .attr('fill', '#000') // Text color
+            .attr('text-anchor', 'middle') // Center the text
+            .text('Date (YYYY-MM-DD)');
+
+        // Add the Y Axis
+        svg.append("g")
+            .call(d3.axisLeft(y))
+            .append('text')
+            .attr('class', 'axis-label')
+            .attr('transform', 'rotate(-90)') // Rotate the text for a vertical label
+            .attr('y', 0 - margin.left) // Position to the left of the y-axis line
+            .attr('x', 0 - (height / 2)) // Center the label vertically
+            .attr('dy', '1em') // Nudge the label down a bit
+            .attr('fill', '#000') // Text color
+            .attr('text-anchor', 'middle') // Center the text
+            .text('Net Worth in USD');
+
+        // Dot and tooltip setup
         const tooltip = d3.select('body').append('div')
             .attr('class', 'tooltip')
             .style('position', 'absolute')
@@ -68,7 +133,7 @@
             .attr("class", "dot")
             .attr("cx", d => x(d.date))
             .attr("cy", d => y(d.value))
-            .attr("r", 5)
+            .attr("r", 3)
             .on("mouseover", (event, d) => {
                 tooltip.transition()
                     .duration(200)
@@ -82,34 +147,6 @@
                     .duration(500)
                     .style("opacity", 0);
             });
-
-        // Draw line
-        svg.append("path")
-            .data([data])
-            .attr("class", "line")
-            .attr("d", line)
-            .style("fill", "none")
-            .style("stroke", "steelblue")
-            .style("stroke-width", "2px");
-
-        // Add the X Axis
-        svg.append('g')
-            .attr('transform', `translate(0,${height})`)
-            .call(d3.axisBottom(x).tickFormat(d3.timeFormat('%Y-%m-%d')));
-
-        // Add the Y Axis
-        svg.append("g")
-            .call(d3.axisLeft(y));
-
-        // Add a neutral line at 0 net worth
-        svg.append("line")
-            .attr("x1", 0)
-            .attr("x2", width)
-            .attr("y1", y(0))
-            .attr("y2", y(0))
-            .attr("stroke-width", 2)
-            .attr("stroke", "black")
-            .style("stroke-dasharray", ("3, 3"));
     }
 
     function initializePlaid() {
