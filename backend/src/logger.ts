4242d33a4ec0e7970013d0bef6b469ddd783b7fc
reidfluegel <reid.fluegel@gmail.com>
2024-04-12 11:57:34 -0500

    revert: mask PLAID_SECRET in logs

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -5,17 +5,10 @@ cls.getNamespace('global')
 const createLogger = () => {
     return winston.createLogger({
         level: 'info',
-        format: winston.format.combine(winston.format.timestamp(), maskSensitiveInfo(), winston.format.json()),
+        format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
         transports: [new winston.transports.Console()]
     })
 }
-
-const maskSensitiveInfo = winston.format((info) => {
-    if (info.message?.message?.config?.headers['PLAID-SECRET']) {
-        info.message.message.config.headers['PLAID-SECRET'] = '********';
-    }
-    return info;
-});
 
 export const logger = createLogger()
 
